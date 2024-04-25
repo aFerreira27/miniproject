dbd2b5732a9be5cbed6112d26e55f1f090a6310b
aFerreira27 <90414431+aFerreira27@users.noreply.github.com>
2024-04-24 22:03:39 -0400

    AI gods

--- MiniProjectPath3.py
+++ MiniProjectPath3.py
@@ -24,19 +24,30 @@ def dataset_searcher(number_list,images,labels):
   #insert code that when given a list of integers, will find the labels and images
   #and put them all in numpy arrary (at the same time, as training and testing data)
 
-  pass
-  #return images_nparray, labels_nparray
+  images_nparray = np.empty((0, *images.shape[1:]), dtype=images.dtype)  # Initialize as empty numpy array
+  labels_nparray = np.empty(0, dtype=labels.dtype)
+
+  for i, number in enumerate(labels):
+        if number in number_list:
+            images_nparray = np.concatenate((images_nparray, [images[i]]), axis=0)  # Append image
+            labels_nparray = np.append(labels_nparray, number)  # Append label
+    
+  return images_nparray, labels_nparray
 
 def print_numbers(images,labels):
   #insert code that when given images and labels (of numpy arrays)
   #the code will plot the images and their labels in the title. 
-  pass
+  for i, image in enumerate(images):
+        plt.title(f"Image #{i+1}, class {labels[i]}")
+        plt.imshow(image, cmap='gray')
+        plt.axis('off')  # Turn off axis
+        plt.show()
 
 class_numbers = [2,0,8,7,5]
 #Part 1
-class_number_images , class_number_labels = dataset_searcher(class_numbers)
+class_number_images , class_number_labels = dataset_searcher(class_numbers, images, labels)
 #Part 2
-print_numbers(class_number_images , class_number_labels )
+#print_numbers(class_number_images , class_number_labels)
 
 
 model_1 = GaussianNB()
@@ -49,34 +60,60 @@ X_train_reshaped = X_train.reshape(X_train.shape[0], -1)
 #Now we can fit the model
 model_1.fit(X_train_reshaped, y_train)
 #Part 3 Calculate model1_results using model_1.predict()
-model1_results = model_1.predict('''What should go in here? Hint, look at documentation and some reshaping may need to be done''')
+model1_results = model_1.predict(X_test.reshape(len(X_test), -1))
 
 
 def OverallAccuracy(results, actual_values):
-  #Calculate the overall accuracy of the model (out of the predicted labels, how many were correct?)
-  
+  correct = 0
+  for i, prediction in enumerate(results):
+      if prediction == actual_values[i]:
+          correct += 1
+
+  Accuracy = correct / len(actual_values)  
   return Accuracy
 
 
 # Part 4
 Model1_Overall_Accuracy = OverallAccuracy(model1_results, y_test)
-print("The overall results of the Gaussian model is " + str(Model1_Overall_Accuracy))
+print("The overall results of the Gaussian model (Test Data) is " + str(Model1_Overall_Accuracy))
 
 
 #Part 5
 allnumbers = [0,1,2,3,4,5,6,7,8,9]
-allnumbers_images, allnumbers_labels = dataset_searcher(allnumbers)
+allnumbers_images, allnumbers_labels = dataset_searcher(allnumbers, images, labels)
 
+model1_results = model_1.predict(allnumbers_images.reshape(len(allnumbers_images), -1))
+Model1_Overall_Accuracy = OverallAccuracy(model1_results, allnumbers_labels)
+print("The overall results of the Gaussian model (ALL NUMBERS) is " + str(Model1_Overall_Accuracy))
 
 
 #Part 6
 #Repeat for K Nearest Neighbors
 model_2 = KNeighborsClassifier(n_neighbors=10)
 
+model_2.fit(X_train_reshaped, y_train)
+model2_results = model_2.predict(X_test.reshape(len(X_test), -1))
+
+Model2_Overall_Accuracy = OverallAccuracy(model2_results, y_test)
+print("The overall results of the K Nearest Neighbors model (Test Data) is " + str(Model2_Overall_Accuracy))
+
+model2_results = model_2.predict(allnumbers_images.reshape(len(allnumbers_images), -1))
+Model2_Overall_Accuracy = OverallAccuracy(model2_results, allnumbers_labels)
+print("The overall results of the K Nearest Neighbors model (ALL NUMBERS) is " + str(Model2_Overall_Accuracy))
+
 
 #Repeat for the MLP Classifier
 model_3 = MLPClassifier(random_state=0)
 
+model_3.fit(X_train_reshaped, y_train)
+model3_results = model_3.predict(X_test.reshape(len(X_test), -1))
+
+Model3_Overall_Accuracy = OverallAccuracy(model3_results, y_test)
+print("The overall results of the MLP Classifier model (Test Data) is " + str(Model3_Overall_Accuracy))
+
+model3_results = model_3.predict(allnumbers_images.reshape(len(allnumbers_images), -1))
+Model3_Overall_Accuracy = OverallAccuracy(model3_results, allnumbers_labels)
+print("The overall results of the MLP Classifier model (ALL NUMBERS) is " + str(Model3_Overall_Accuracy))
 
 
 #Part 8
@@ -99,7 +136,7 @@ X_train_poison = X_train + poison
 # When fitting the KernelPCA method, the input image of size 8x8 should be reshaped into 1 dimension
 # So instead of using the X_train_poison data of shape 718 (718 images) by 8 by 8, the new shape would be 718 by 64
 
-X_train_denoised = # fill in the code here
+#X_train_denoised = # fill in the code here
 
 
 #Part 14-15
